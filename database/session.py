6e2e8fbbdbff701605ff2f5840444ba42bb98ad1
ChengYi0224 <klps0224@gmail.com>
2025-05-02 16:12:33 +0800

    更新SQL ORM base

--- database/session.py
+++ database/session.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from backend.app.models.base import Base
 
 DB_PASSWORD = "MusicDBPassword"
 DB_URL = f"postgresql://postgres:{DB_PASSWORD}@db.jhdsifyqyxgrioeuloef.supabase.co:5432/postgres"
 
 engine = create_engine(DB_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
