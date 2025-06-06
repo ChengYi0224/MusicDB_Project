b6e052f4af87d034e03ce412b0be6674a4c41ebe
Barbiehuang0603 <41247063S@gapps.ntnu.edu.tw>
2025-06-07 04:43:59 +0800

    app中強制IPv6檢查

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
 from app import create_app
+from app.utils import Has_IPv6_Addr
 
 # 程式入口
 if __name__ == "__main__":
-    app = create_app()
+    if Has_IPv6_Addr():
+        app = create_app()
 
-    # 啟動伺服器
-    app.run(debug=True)
+        # 啟動伺服器
+        app.run(debug=True)
+    else:
+        print("您的系統不支援 IPv6，請使用WARP或其他方式啟用 IPv6。")
+        exit(1)
