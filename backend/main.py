6cd4c6a57990b1972d447520553126e7fadbce6e
ChengYi0224 <klps0224@gmail.com>
2025-06-07 04:56:51 +0800

    更新app強制IPv6邏輯

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,17 @@
-from app import create_app
+from app import create_app#, db
+from flask_migrate import Migrate
 from app.utils import Has_IPv6_Addr
 
 # 程式入口
 if __name__ == "__main__":
-    if Has_IPv6_Addr():
-        app = create_app()
-
-        # 啟動伺服器
-        app.run(debug=True)
-    else:
-        print("您的系統不支援 IPv6，請使用WARP或其他方式啟用 IPv6。")
-        exit(1)
+    app = create_app()
+    #migrate = Migrate(app, db)
+    if not Has_IPv6_Addr():
+        print("本機不支援 IPv6，強制啟動將無法連線資料庫。")
+        print("是否強制啟動？(y/n): ", end="")
+        force = input().strip().lower() == 'y'
+        if not force:
+            exit(1)
+    
+    app.run(debug=True)
+    
